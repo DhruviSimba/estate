00abf007fcd29cada4a885853aa2c1e8ed8043bc
DhruviSimba <dhruvi.simba@gmail.com>
2024-01-01 19:12:14 +0530

    SignUp & SignIn UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import SignIn from './pages/SignIn';
 
-function App() {
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+export default function App() {
+  return <Router>
+    <Header></Header>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
+      <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/profile' element={<Profile></Profile>}></Route>
+    </Routes>
+  </Router>
 }
-
-export default App
